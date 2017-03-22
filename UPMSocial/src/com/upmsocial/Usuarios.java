a3956c3fa47b7a7bab4898d9ca9de5538e04a02e
Diego Fernández <diegofpb93@gmail.com>
2017-03-22 19:12:42 +0100

    Example to show how to get Users from DB.

--- UPMSocial/src/com/upmsocial/Usuarios.java
+++ UPMSocial/src/com/upmsocial/Usuarios.java
@@ -1,5 +1,11 @@
 package com.upmsocial;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -18,8 +24,19 @@ public class Usuarios {
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response sayPlainTextHello() {
-		String respuesta = "Hello Jersey";
+	public Response sayPlainTextHello() throws SQLException {
+		
+		BBDD newQuery = new BBDD();
+		List<Map<String, Object>> result = newQuery.ExecuteQuery("SELECT * FROM RestBBDD.USERS");
+		
+		String respuesta = "";
+		
+		for (Map<String, Object> map : result) {
+			 
+			respuesta = respuesta+(String) map.get("username")+"\n";
+		}
+		
+		
 	    return Response.status(Response.Status.OK).entity(respuesta).
 	    		header("Content Location", uriInfo.getAbsolutePath().toString()).build();
 
