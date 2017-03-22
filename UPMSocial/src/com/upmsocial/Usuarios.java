6696fe7508676058fc82d70deff4425a8927c164
Diego Fernández <diegofpb93@gmail.com>
2017-03-22 12:32:02 +0100

    Testing with headers

--- UPMSocial/src/com/upmsocial/Usuarios.java
+++ UPMSocial/src/com/upmsocial/Usuarios.java
@@ -3,17 +3,25 @@ package com.upmsocial;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
 
 @Path("/usuarios")
 public class Usuarios {
+	
+	@Context
+	private UriInfo uriInfo;
+
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public Response sayPlainTextHello() {
 		String respuesta = "Hello Jersey";
-	    return Response.status(Response.Status.OK).entity(respuesta).build();
+	    return Response.status(Response.Status.OK).entity(respuesta).
+	    		header("Content Location", uriInfo.getAbsolutePath().toString()).build();
 
 	}
 
