2ed768e772e41c0bb3d83a0a710158971fd454a2
Diego Fernández <diegofpb93@gmail.com>
2017-03-23 18:44:58 +0100

    get username/{id} now works

--- UPMSocial/src/com/upmsocial/Usuarios.java
+++ UPMSocial/src/com/upmsocial/Usuarios.java
@@ -1,12 +1,15 @@
 package com.upmsocial;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import com.upmsocial.bbdd.BBDD;
@@ -22,13 +25,37 @@ public class Usuarios {
 
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public List<TipoUser> getUsers() throws SQLException, ClassNotFoundException {
+	public List<TipoUser> getUsers() throws ClassNotFoundException, SQLException  {
 		
 		BBDD bdconn = new BBDD();
 		List<TipoUser> users = bdconn.Usuarios();
-		
 	    return users;
 	}
+	
+	@GET
+	@Path("{username}")
+    @Produces(MediaType.APPLICATION_XML)
+	
+    public Response getUser(@PathParam("username") String username) throws ClassNotFoundException, SQLException {
+		
+		BBDD bdconn = new BBDD();
+		ResultSet res = bdconn.Usuario(username); 
+		
+		TipoUser User = new TipoUser();
+		 
+		if (!res.next()) {        
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}else{
+			User.setNombre(res.getString(1));
+			User.setSurname(res.getString(2));
+			User.setUsername(res.getString(3));					
+		}
+		
+        return Response.status(Response.Status.OK).entity(User)
+        .header("Location", uriInfo.getAbsolutePath().toString()).build();
+    }
+	
+	
 
 }
 
