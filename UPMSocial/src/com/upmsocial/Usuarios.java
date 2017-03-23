c2b726749345d70d2b3635c614785e1adf61e880
Diego Fernández <diegofpb93@gmail.com>
2017-03-23 17:39:26 +0100

    Endpoint de Usuarios.

--- UPMSocial/src/com/upmsocial/Usuarios.java
+++ UPMSocial/src/com/upmsocial/Usuarios.java
@@ -2,18 +2,15 @@ package com.upmsocial;
 
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import com.upmsocial.bbdd.BBDD;
+import com.upmsocial.models.TipoUser;
 
 
 @Path("/usuarios")
@@ -24,23 +21,13 @@ public class Usuarios {
 
 
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response sayPlainTextHello() throws SQLException {
+	@Produces(MediaType.APPLICATION_XML)
+	public List<TipoUser> getUsers() throws SQLException, ClassNotFoundException {
 		
-		BBDD newQuery = new BBDD();
-		List<Map<String, Object>> result = newQuery.ExecuteQuery("SELECT * FROM RestBBDD.USERS");
+		BBDD bdconn = new BBDD();
+		List<TipoUser> users = bdconn.Usuarios();
 		
-		String respuesta = "";
-		
-		for (Map<String, Object> map : result) {
-			 
-			respuesta = respuesta+(String) map.toString()+"\n";
-		}
-		
-		
-	    return Response.status(Response.Status.OK).entity(respuesta).
-	    		header("Content Location", uriInfo.getAbsolutePath().toString()).build();
-
+	    return users;
 	}
 
 }
