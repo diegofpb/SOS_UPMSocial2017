8d5e32693d745baedc8a090414b54069224c4e54
DESKTOP-U1PDHI8\Monty <monty_030@hotmail.com>
2017-03-24 16:26:36 +0100

    Class Post

---
+++ UPMSocial/src/com/upmsocial/Post.java
@@ -0,0 +1,43 @@
+package com.upmsocial;
+
+import java.sql.SQLException;
+import java.util.Date;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import com.upmsocial.bbdd.BBDD;
+import com.upmsocial.models.TipoPost;
+import com.upmsocial.models.User;
+
+@Path("/Post")
+public class Post {
+	
+	@Context
+	private UriInfo uriInfo;
+	
+	 @POST
+	 @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	 public Response addPost(
+	         @FormParam("username") String username,
+	         @FormParam("date") Date date,
+	         @FormParam("url") String url,
+	         @FormParam("description") String description) throws ClassNotFoundException, SQLException {
+		 
+		 BBDD con = new BBDD();
+		 int id = con.newIdPost();
+		 
+		 TipoPost post = new TipoPost(id, username, date, url, description);
+	     
+	     return con.addPost(post,uriInfo);
+	 }
+
+}
