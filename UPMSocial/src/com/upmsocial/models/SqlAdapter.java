3e3647949badd5cf32946551ede3d2a48d5ccde6
DESKTOP-U1PDHI8\Monty <monty_030@hotmail.com>
2017-03-29 01:36:12 +0200

    conversor de tipos java.until.date a java.sql.date

---
+++ UPMSocial/src/com/upmsocial/models/SqlAdapter.java
@@ -0,0 +1,8 @@
+package com.upmsocial.models;
+
+public class SqlAdapter {
+	
+	java.util.Date utilDate = new java.util.Date();
+    java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+
+}
